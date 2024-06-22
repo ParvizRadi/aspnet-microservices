002b989e264e29f8b03ac3620397ef500b82826a
parviz_radi <parviz_radi@yahoo.com>
2024-06-22 23:31:29 +0330

    Task #20240622 - Catalog - Docker

--- Services/Catalog/Catalog.Api/Data/CatalogContext.cs
+++ Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -9,10 +9,10 @@ namespace Catalog.Api.Data
 
         public CatalogContext(IConfiguration configuration)
         {
-            var client = new MongoClient(configuration.GetConnectionString("CatalogConnectionString"));
-            var database = client.GetDatabase(configuration.GetValue<string>("ConnectionStrings:DatabaseName"));
+            var client = new MongoClient(configuration.GetValue<string>("DatabaseSettings:ConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("DatabaseSettings:DatabaseName"));
 
-            Products = database.GetCollection<Product>(configuration.GetValue<string>("ConnectionStrings:CollectionName"));
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("DatabaseSettings:CollectionName"));
 
             CatalogContextSeed.SeedData(Products);
         }

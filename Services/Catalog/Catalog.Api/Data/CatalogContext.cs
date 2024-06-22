234282cff91785a3259e3914c9d32258da10640f
parviz_radi <parviz_radi@yahoo.com>
2024-06-22 13:04:48 +0330

    Task #20240622 - Add Context & Product & seed data

---
+++ Services/Catalog/Catalog.Api/Data/CatalogContext.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContext : ICatalogContext
+    {
+        public IMongoCollection<Product> Products { get; set; }
+
+        public CatalogContext(IConfiguration configuration)
+        {
+            var client = new MongoClient(configuration.GetConnectionString("CatalogConnectionString"));
+            var database = client.GetDatabase(configuration.GetValue<string>("ConnectionStrings:DatabaseName"));
+
+            Products = database.GetCollection<Product>(configuration.GetValue<string>("ConnectionStrings:CollectionName"));
+
+            CatalogContextSeed.SeedData(Products);
+        }
+
+    }
+}
---
+++ Services/Catalog/Catalog.Api/Data/CatalogContextSeed.cs
@@ -0,0 +1,47 @@
+﻿using Catalog.Api.Entities;
+using MongoDB.Driver;
+
+namespace Catalog.Api.Data
+{
+    public class CatalogContextSeed
+    {
+        public static void SeedData(IMongoCollection<Product> products)
+        {
+            bool existsProduct = products
+                .Find(p => true)
+                .Any();
+
+            if (existsProduct)
+            {
+                products.InsertManyAsync(GetSeedData());
+            }
+        }
+
+        private static IEnumerable<Product> GetSeedData()
+        {
+            return new List<Product>()
+            {
+                new Product
+                {
+                    Id = "602d2149e773f2a3990b47f5",
+                    Name = "IPhone X",
+                    Summary = "Test Summary",
+                    Description = "Test Description",
+                    ImageFile = "IPhone.jpg",
+                    Category = "Mobile",
+                    Price = 499,
+                },
+                new Product
+                {
+                    Id = "602d2149e773f2a3990b47f6",
+                    Name = "Samsung A70",
+                    Summary = "Test Summary",
+                    Description = "Test Description",
+                    ImageFile = "Sumsung.jpg",
+                    Category = "Mobile",
+                    Price = 399,
+                }
+            };
+        }
+    }
+}

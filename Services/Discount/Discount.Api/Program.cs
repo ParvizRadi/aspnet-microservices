fe91a05a37c20f4ba98c40fe588d76d2efff04ed
parviz_radi <parviz_radi@yahoo.com>
2024-06-23 16:28:17 +0330

    Task #20240623 - Discount - Add Migration & Bug fixes

--- Services/Discount/Discount.Api/Program.cs
+++ Services/Discount/Discount.Api/Program.cs
@@ -1,3 +1,4 @@
+using Discount.Api.Extensions;
 using Discount.Api.Repositories;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -11,6 +12,7 @@ builder.Services.AddSwaggerGen();
 builder.Services.AddScoped<IDiscountRepository, DiscountRepository>();
 
 var app = builder.Build();
+app.MigrateDatabase<Program>();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())

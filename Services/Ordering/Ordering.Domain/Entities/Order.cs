950bf69715200b1dd0621a432c20931872ad2842
parviz_radi <parviz_radi@yahoo.com>
2024-06-24 15:53:31 +0330

    Task #20240624- Ordering - Add Project with DDD Architecture

---
+++ Services/Ordering/Ordering.Domain/Entities/Order.cs
@@ -0,0 +1,33 @@
+﻿using Ordering.Domain.Common;
+
+namespace Ordering.Domain.Entities
+{
+    public class Order : EntityBase
+    {
+        // todo: validation's
+
+        public string UserName { get; set; }
+
+        public decimal TotalPrice { get; set; }
+
+        //address
+
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public string EmailAddress { get; set; }
+
+        public string Country { get; set; }
+        public string City { get; set; }
+        public string ZipCode { get; set; }
+
+        //payment
+
+        public string BankName { get; set; }
+        public string RefCode { get; set; }
+
+        public byte PaymentMethod { get; set; }
+
+
+    }
+}
